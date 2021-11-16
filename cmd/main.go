c1f1c950c52e1f000a65ff7c6f2c46c4983ad455
hardstifler <hardstifler@gmail.com>
2021-11-16 15:40:10 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main 
 
-import "fmt"
+
+import (
+  "fmt"
+  "github.com/hardstifler/routinePool/version"
+ )
+
 
 func main(){
-
-
+  fmt.Println(version.GitVersion)
+	fmt.Println(version.BuildTimeStamp)
+	fmt.Println(version.OS)
 }
