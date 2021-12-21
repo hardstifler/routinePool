539405c057a3fa827f765d891a0d2c4ae0f615b6
hardstifler <hardstifler@gmail.com>
2021-12-21 20:53:47 +0800

    Create generic.go
    golang 1.8 泛型初探

---
+++ generic.go
@@ -0,0 +1,99 @@
+package main
+
+import "fmt"
+
+type MyNumber interface {
+	int |int32|float64
+}
+
+func main() {
+	var m1 map[string]int = map[string]int{
+		"1": 1,
+		"2": 2,
+		"3": 3,
+	}
+
+	var m2 map[string]float64 = map[string]float64{
+		"1": 1.1,
+		"2": 2.2,
+		"3": 3.3,
+	}
+
+	sum1 := addInt(m1)
+	sum2 := addFloat(m2)
+	fmt.Println(sum1)
+	fmt.Println(sum2)
+
+	fmt.Println(addWithGeneric(m1))
+	fmt.Println(addWithGeneric(m2))
+  
+  //目前泛型约束不能省略
+  //居然必须手动指定类型，希望后续有更智能的类型推导
+	t1 := &GenericStruct[bool]{
+		Value:true,
+		Elem:"hello",
+	}
+
+	t2 := &GenericStruct[string]{
+		Value:"hello",
+		Elem:"hello",
+	}
+
+	fmt.Println(t1.GetValue())
+	fmt.Println(t2.GetValue())
+
+
+	intSlice := []int{1,2,3,4,5}
+	floatSlice := []float64{1.1,2.2,3.3, 4.4}
+	fmt.Println(Max(intSlice...))
+	fmt.Println(Max(floatSlice...))
+}
+
+func addInt(args map[string]int) int {
+	var sum int
+	for _, v := range args {
+		sum += v
+	}
+	return sum
+}
+
+func addFloat(args map[string]float64) float64 {
+	var sum float64
+	for _, v := range args {
+		sum += v
+	}
+	return sum
+}
+
+func addWithGeneric[K comparable, V MyNumber](args map[K]V) V{
+	var sum V
+	for _, v := range args {
+		sum += v
+	}
+	return sum
+}
+
+
+type GenericStruct[T any] struct{
+	Value T  `json:"value"`
+	Elem string  `json:"elem"`
+}
+
+
+func (e *GenericStruct[T])GetValue()T{
+	return e.Value
+}
+
+type GenericSlice[T any] []T 
+
+type GenericMaps[K comparable, V any] map[K]V 
+
+func Max[T MyNumber](elem ...T)T{
+	var max T 
+	for _,v := range elem {
+		if v > max{
+			max = v 
+		}
+	}
+	return max
+}
